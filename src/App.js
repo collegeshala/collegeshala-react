0973ceaf8d6b6e364d0c7bc46836fb84fa4352de
MuJtaba Basheer <mujtaba.fleapo@gmail.com>
2020-05-18 15:27:51 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import test from "./js/test"
 
 function App() {
   return (
@@ -9,16 +10,18 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {test("WoRlD")}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="spa.html"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
       </header>
+      <script src="./../public/test1.js" />
     </div>
   );
 }
