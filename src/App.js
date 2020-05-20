b2b70bcf354cadcb844453ced25781a572e32994
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2020-05-20 14:14:26 +0530

    Added Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import StudentAccount from "./components/Student/Account";
+import React, {Component} from 'react';
+import './assets/css/style.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Header from './components/Global/Header';
+import Footer from './components/Global/Footer';
+import Copyright from './components/Global/pages/Copyright'
 
-function App() {
-  return (
-    <StudentAccount />
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+        <Header />
+          <Switch>
+            <Route exact path="/copyright" component={Copyright} />
+          </Switch>
+        <Footer />
+      </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Global/pages/Copyright.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Copyright extends Component {
+  render() {
+    return (
+      <body>
+        <div class="container text-center py-3">
+          <h2>COPYRIGHT POLICY</h2>
+          <div className="text-left">
+            <p>All pages, data, and graphics presented on this website are the property of Collegeshala or the Author of the content as specified.<br/><br/>
+              The pages and contents may not be redistributed or reproduced in any way, shape, or form without the written permission of Collegeshala or the Author of the content.<br/><br/>
+              Collegeshala respects the copyrights, trademarks and intellectual property of others and we expect this from other users. If you find any information that is owned by you or any content that violates your intellectual property rights, please contact us at collegeshala2020@gmail.com with all necessary documents/information that authenticates your authority on your property.<br/><br/>
+              For all other policies, please read our Terms of use.
+              </p>
+          </div>
+       </div>
+    </body>
+    )
+  }
+}
+
+export default Copyright;
