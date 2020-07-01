e18633fa9dd6923827780f08a5d8264d89f87ad4
Richard Henry Rozario <richardrozario.rr@gmail.com>
2020-07-01 18:48:08 +0530

    reset

--- src/js/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  cognito: {
-    userPoolId: "ap-south-1_U4s2cmPdJ",
-    userPoolClientId: "v5519lmtnembdvh0r6tp15tts",
-    region: "ap-south-1",
-  },
-};
-
-export default config;
