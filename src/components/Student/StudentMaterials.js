2daf5aebb3f5e88058587e8d38f12ae3f8fd9a2e
Richard Henry Rozario <richardrozario.rr@gmail.com>
2020-07-18 12:56:58 +0530

    student materials text

--- src/components/Student/StudentMaterials.js
+++ src/components/Student/StudentMaterials.js
@@ -102,10 +102,10 @@ class Materials extends React.Component {
                             <h5 className="professor">
                               By {note.professorname}
                             </h5>
-                            <p className="card-text">
+                            {/* <p className="card-text">
                               Some quick example text to build on the card title
                               and make up the bulk of the card's content.
-                            </p>
+                            </p> */}
                             <button
                               onClick={(event) => {
                                 event.preventDefault();
