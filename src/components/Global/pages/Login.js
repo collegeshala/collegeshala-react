ccbe35f858454bd8ced48c37dee92ce1fdc7f810
RupdeepSaha <rupdeep176@hotmail.com>
2020-07-20 17:30:09 +0530

    Checking if button overlapping with footer is resolved or not

--- src/components/Global/pages/Login.js
+++ src/components/Global/pages/Login.js
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import Navbar from "../Navbar";
 import Footer from "../Footer";
 
 import { navigate } from "@reach/router";
